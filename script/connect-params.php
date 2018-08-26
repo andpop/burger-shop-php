b927f810e0e61bc0db441677a3847e101d7f0a27
Andrey Popov <andpop@mail.ru>
2018-08-27 00:58:53 +0300

    Correct character set in DBMS, DB, tables and columns.

---
+++ script/connect-params.php
@@ -0,0 +1,5 @@
+<?php
+define('MYSQL_SERVER', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', '12345678');
+define('MYSQL_DB', 'burgers');
--- script/process-order.php
+++ script/process-order.php
@@ -3,6 +3,9 @@ header('Content-type: text/html');
 header('Access-Control-Allow-Origin: *');
 // Без этих заголовков скрипт на хостинге не загружается!
 
+require_once 'connect-params.php';
+require_once 'functions.php';
+
 /**
  * Проверка заполненности полей формы заказа
  */
@@ -56,12 +59,8 @@ function checkFields()
     }
 };
 
-function db_connect()
+function dbConnectMySqli()
 {
-    define('MYSQL_SERVER', 'localhost');
-    define('MYSQL_USER', 'root');
-    define('MYSQL_PASSWORD', '12345678');
-    define('MYSQL_DB', 'burgers');
 
     $mysqli = new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
 
@@ -74,30 +73,90 @@ function db_connect()
     }
 
     return $mysqli;
-}
-
-function authorization($email)
-{
-    $query = "SELECT * FROM users WHERE `email`='$email''";
-    echo $query;
 };
 
+function dbConnectPDO()
+{
+  $dsn = "mysql:host=localhost;dbname=burgers;charset=utf8";
+  $pdo = new PDO($dsn, MYSQL_USER, MYSQL_PASSWORD);
+  $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $pdo;
+};
+
+/**
+ * Авторизация клиента по наличию в базе его email
+ * @param $email - email проверяемого пользователя (должен быть уникальным)
+ */
+function isNewUser($email)
+{
+    global $pdo;
+    $prepare = $pdo->prepare('SELECT * FROM users where `email` = :email');
+    $prepare->execute(['email' => $email]);
+    $data = $prepare->fetchAll(PDO::FETCH_OBJ);
+
+    return (count($data) == 0);
+};
+
+/**
+ * Добавление нового клиента в таблицу users
+ * @param $email
+ * @param $name
+ * @param $phone
+ */
 function addUser($email, $name, $phone)
 {
-    $query = "INSERT INTO users (email, name, phone) VALUES ('{$email}', '{$name}', '{$phone}')";
-    echo $query;
+    global $pdo;
+    $prepare = $pdo->prepare('INSERT INTO `users` (email, name, phone) VALUES (:email, :name, :phone)');
+    $prepare->execute(['email' => $email, 'name' => $name, 'phone' => $phone]);
 };
+
+function authorizeUser($email, $name, $phone)
+{
+  if (isNewUser($email)) {
+      addUser($email, $name, $phone);
+  }
+};
+
+/**
+ * Возвращает id клиента по его email
+ * @param $email
+ * @return mixed - id клиента из таблицы users
+ */
+function getIdUser($email)
+{
+    global $pdo;
+    $prepare = $pdo->prepare('SELECT id FROM users where `email` = :email');
+    $prepare->execute(['email' => $email]);
+    $data = $prepare->fetchAll(PDO::FETCH_ASSOC);
+    return $data[0]['id'];
+};
+
+function addOrder($idUser, $street, $home, $part, $appt, $floor, $comment)
+{
+    global $pdo;
+    $query = 'INSERT INTO `orders` (id_user, street, home, part, appt, floor, comment) VALUES (:idUser, :street, :home, :part, :appt, :floor, :comment)';
+    $prepare = $pdo->prepare($query);
+    dump($prepare);
+
+    $prepareMapping = ['idUser' => $idUser, 'street' => $street, 'home' => $home, 'part' => $part, 'appt' => $appt, 'floor' => $floor, 'comment' => $comment];
+    dump($prepareMapping);
+
+    $prepare->execute(['idUser' => $idUser, 'street' => $street, 'home' => $home, 'part' => $part, 'appt' => $appt, 'floor' => $floor, 'comment' => $comment]);
+    return $pdo->lastInsertId();
+};
+
 //$outMessage = '';
 //$data = [];
 
 //checkFields();
 
 // Подключение к БД
-$mysqli = db_connect();
-$mysqli->set_charset("utf8");
-
-authorization($_POST['email']);
-addUser($_POST['email'], $_POST['name'], $_POST['phone']);
+//$mysqli = dbConnectMySqli();
+//$mysqli->set_charset("utf8");
+$pdo = dbConnectPDO();
+authorizeUser($_POST['email'], $_POST['name'], $_POST['phone']);
+$idUser = getIdUser($_POST['email']);
+$orderNumber = addOrder($idUser, $_POST['street'], $_POST['home'], $_POST['part'], $_POST['appt'], $_POST['floor'], $_POST['comment']);
 
 
 $to = 'andrey@localhost'; // Режим тестирования - шлем на локальный адрес
